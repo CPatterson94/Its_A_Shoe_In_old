6435d946916c41132bae4b8013da55379881089d
10zout <tenzinthunsur@gmail.com>
2024-06-14 10:40:47 -0400

    index

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -8,6 +8,7 @@ client.connect();
 app.use("/api/users", require("./users"));
 app.use("/api/products", require("./products"));
 app.use("/api/cart", require("./cart"));
+app.use("/api/orders", require("./orders"));
 
 app.listen(8080, () => {
   console.log("App is running at port 8080");
