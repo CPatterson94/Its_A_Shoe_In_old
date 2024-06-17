e9e7d27432d5f72b1ad8da9f03eed0defdb44803
ojgraham1 <166427274+ojgraham1@users.noreply.github.com>
2024-06-17 13:01:07 -0400

    Add files via upload

--- src/server/auth/auth.js
+++ src/server/auth/auth.js
@@ -32,7 +32,7 @@ router.post("/login", async (req, res) => {
       return res.status(401).json({ error: "Invalid username or password" });
     }
 
-    const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: "1h" });
+    const token = jwt.sign({ userId: user.id }, jwtSecret, { expiresIn: "1w" });
     res.json({ token });
   } catch (error) {
     console.error("Error logging in:", error);
--- src/server/auth/middleware.js
+++ src/server/auth/middleware.js
@@ -1,12 +1,14 @@
 const jwt = require("jsonwebtoken");
-const jwtSecret = "Whitegyalshoes"; // we can change it if ya wanna
+const jwtSecret = "Whitegyalshoes"; 
+
 
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
 
+  if (!token) return res.sendStatus(401);
+console.log(token, jwtSecret)
   jwt.verify(token, jwtSecret, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
@@ -14,12 +16,13 @@ const authenticateToken = (req, res, next) => {
   });
 };
 
-const isAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) {
-    next();
-  } else {
-    res.sendStatus(403);
-  }
-};
+
+  const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin ===true) {
+      next(); 
+    } else {
+      res.sendStatus(403); 
+    }
+  };
 
 module.exports = { authenticateToken, isAdmin };
